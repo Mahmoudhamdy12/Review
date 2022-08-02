63037f5de6ef76c020eb693d99a6e3fd92988bf2
Mahmoud Hamdy <houdahamdy12@gmail.com>
2022-08-02 18:44:35 +0200

    Updates

---
+++ precache-manifest.953094596c0bb3bbb3c67554e6b5a872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a84b401ae399ed263d8802e06cded66",
+    "url": "/Review/index.html"
+  },
+  {
+    "revision": "7972b1277f6fdd63d106",
+    "url": "/Review/static/css/main.9be6d76e.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/Review/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Review/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7972b1277f6fdd63d106",
+    "url": "/Review/static/js/main.9790b02d.chunk.js"
+  },
+  {
+    "revision": "0af5eb17e83c4890c509",
+    "url": "/Review/static/js/runtime-main.af3c5d78.js"
+  }
+]);
